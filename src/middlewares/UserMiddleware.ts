bb4a38736fa8f8cc17c4530b1b4cb3be4604c159
ThaisFReis <reis.thaisf@gmail.com>
2023-03-22 03:02:29 -0300

    Muitas mudanças foram feitas

--- src/middlewares/UserMiddleware.ts
+++ src/middlewares/UserMiddleware.ts
@@ -1,17 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
-import { verify } from 'jsonwebtoken';
 import { HttpException } from '../utils/HttpException';
-import UserRepository from '../repositories/UserRepository';
 import { createUserSchema, updateUserSchema, loginSchema } from '../schemas/UserSchema';
 
 async function validateCreateUser(req: Request, res: Response, next: NextFunction) {
     try {
-        const { name, email, password } = req.body;
         await createUserSchema.validateAsync(req.body);
-        const user = await UserRepository.getUserByEmail(email);
-        if (user) {
-            next(new HttpException(400, 'User already exists'));
-        }
         next();
     } catch (error) {
         next(new HttpException(400, error.message));
---
+++ tests/integration/user.test.ts
@@ -0,0 +1,76 @@
+import supertest from 'supertest';
+import app, { init } from "../../src/app";
+import { prisma } from "../../src/config";
+import { faker } from "@faker-js/faker";
+import httpStatus from "http-status";
+import { createUser } from "../factories";
+import { cleanDatabase } from "../utils/helpers";
+import UserController from '../../src/controllers/UserController';
+import { signIn } from '../../src/controllers/AuthController';
+
+beforeAll(async () => {
+        await init();
+        await cleanDatabase();
+});
+
+const server = supertest(app);
+
+describe("POST /users/signup", () => {
+
+        it("should respond with status 400 when body is not given", async () => {
+                const response = await server.post("/users/signup");
+
+                expect(response.status).toBe(httpStatus.BAD_REQUEST);
+        });
+
+        it("should respond with status 400 when body is not valid", async () => {
+                const invalidBody = { [faker.lorem.word()]: faker.lorem.word() };
+
+                const response = await server.post("/users/signup").send(invalidBody);
+
+                expect(response.status).toBe(httpStatus.BAD_REQUEST);
+        });
+
+        describe("when body is valid", () => {
+                const validBody = {
+                        name: faker.name.fullName(),
+                        email: faker.internet.email(),
+                        password: faker.internet.password(6),
+                };
+
+                it("should respond with status 201 and create user when given email is unique", async () => {
+                        const response = await server.post("/users/signup").send(validBody);
+              
+                        expect(response.status).toBe(httpStatus.CREATED);
+                        expect(response.body).toEqual({
+                                name: validBody.name,
+                                email: validBody.email,
+                        });
+                });
+
+                it("should save user on db", async () => {
+                        const response = await server.post("/users/signup").send(validBody);
+
+                        const user = await prisma.user.findFirst({
+                                where: { email: response.body.email },
+                        });
+
+                        expect(user).not.toBeNull();
+
+                });
+
+                it("should respond with status 400 and not create user when given email is not unique", async () => {
+                        await createUser(validBody);
+
+                        const response = await server.post("/users/signup").send(validBody);
+
+                        expect(response.status).toBe(httpStatus.BAD_REQUEST);
+                });
+
+                it("should not return user password on body", async () => {
+                        const response = await server.post("/users/signup").send(validBody);
+
+                        expect(response.body.password).toBeUndefined();
+                });
+        });
+});
